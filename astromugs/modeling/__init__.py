fe509931ec8f2686dd439068d2f6f34ed0de595b
sachagavino <sacha.gavino@gmail.com>
2026-03-09 00:15:33 +0100

    rename Structure.py to Interface.py

--- astromugs/modeling/__init__.py
+++ astromugs/modeling/__init__.py
@@ -2,7 +2,7 @@ from . Star import Star
 from . Grid import Grid
 from . Disk import Disk
 from . Envelope import Envelope
-from . Structure import Structure
+from . Interface import Interface
 from .Model import Model
 from . InterstellarRadFields import InterstellarRadFields
 
